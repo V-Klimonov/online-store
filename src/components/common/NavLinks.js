2d26a87f21a549f46d6ebec1fe0334053460af71
V-Klimonov <822775v@gmail.com>
2022-04-03 15:09:45 +0300

    add active linc css

---
+++ src/components/common/NavLinks.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+import { useQuery } from "@apollo/client";
+import { LINKS_NAME } from "../../graphQl/query/links";
+
+const StyledNavLink = styled(NavLink)`
+  font-family: Raleway;
+  text-transform: uppercase;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 19.2px;
+  padding: 16px;
+  text-decoration: none;
+  color: #1d1f22;
+  &.active {
+    color: #5ece7b;
+    border-bottom: 3px solid #5ece7b;
+  }
+`;
+
+const NavLinks = () => {
+  const { loading, error, data } = useQuery(LINKS_NAME);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error ...</p>;
+  const { categories } = data;
+  return (
+    <>
+      {categories.map((el) => {
+        return (
+          <StyledNavLink to={`category/${el.name}`} key={el.name}>
+            {el.name}
+          </StyledNavLink>
+        );
+      })}
+    </>
+  );
+};
+
+export default NavLinks;
--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,49 +1,29 @@
-import { useQuery } from "@apollo/client";
 import React from "react";
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
-import { LINKS_NAME } from "../../graphQl/query/links";
+import NavLinks from "./NavLinks";
+
 const Logo = styled(NavLink)`
   font-family: "Kaushan Script", cursive;
   font-size: 34px;
   color: #5ece7b;
   text-decoration: none;
+  margin-right: 40px;
 `;
 
 const NavbarWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-around;
-`;
-
-const StyledNavLink = styled(NavLink)`
-  font-family: Raleway;
-  text-transform: uppercase;
-  font-weight: 600;
-  font-size: 16px;
-  line-height: 19.2px;
-  padding: 16px;
-  text-decoration: none;
+  height: 80px;
 `;
 
 const Navbar = () => {
-  const { loading, error, data } = useQuery(LINKS_NAME);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error ...</p>;
-  const { categories } = data;
-  console.log(categories);
   return (
     <NavbarWrapper>
       <div>
-        <Logo to="/">Lifestyle idea's</Logo>
-        {categories.map((el) => {
-          return (
-            <StyledNavLink to={`category/${el.name}`} key={el.name}>
-              {el.name}
-            </StyledNavLink>
-          );
-        })}
+        <Logo to="/">iShop</Logo>
+        <NavLinks />
       </div>
     </NavbarWrapper>
   );
